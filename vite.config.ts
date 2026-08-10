dc356bef8f6babe895681f23e93698804f9eb40b
rail2025 <undrln@gmail.com>
2026-08-10 03:00:24 -0700

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+
+// @ts-expect-error process is a Node.js global
+const host = process.env.TAURI_DEV_HOST;
+
+export default defineConfig(async () => ({
+    clearScreen: false,
+
+    server: {
+        port: 1420,
+        strictPort: true,
+        host: host || false,
+        hmr: host
+            ? {
+                protocol: "ws",
+                host,
+                port: 1421,
+            }
+            : undefined,
+        watch: {
+            ignored: ["**/src-tauri/**"],
+        },
+    },
+
+    build: {
+        rollupOptions: {
+            input: {
+                main: "index.html",
+                map: "map.html",
+            },
+        },
+    },
+}));
